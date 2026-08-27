0a1a55757dd86dee393741761de74d08ee4c0366
Easha Angali <eashaangalikhatri@gmail.com>
2026-08-27 04:08:22 -0700

    Add user entity

--- src/main/java/com/example/courseenrollmentsystem/Entity/User.java
+++ src/main/java/com/example/courseenrollmentsystem/Entity/User.java
@@ -1,4 +1,16 @@
 package com.example.courseenrollmentsystem.Entity;
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Userid;
+    @Column(unique = true, nullable = false)
+    private String Username;
+    @Column(nullable = false)
+    private String Password;
+    @Column(unique = true, nullable = false)
+    private String Email;
+    private boolean enabled = true;
+    private boolean AccountNotBlocked = true;
+
 }
