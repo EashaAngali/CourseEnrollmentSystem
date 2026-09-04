c2ee9e3869426e2816f9abfea30d1c4959e64416
Easha Angali <eashaangalikhatri@gmail.com>
2026-09-04 12:52:21 -0700

    Create user repository interface

---
+++ src/main/java/com/example/courseenrollmentsystem/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.courseenrollmentsystem.Repository;
+
+import com.example.courseenrollmentsystem.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
